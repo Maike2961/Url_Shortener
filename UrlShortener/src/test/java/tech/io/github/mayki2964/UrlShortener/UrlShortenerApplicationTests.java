a85d7f60ef12aa355cd7340a4c38eedf12829f81
Maike2961 <mayki.silva@hotmail.com>
2025-05-16 21:36:42 -0300

    chore: application configs

---
+++ UrlShortener/src/test/java/tech/io/github/mayki2964/UrlShortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.io.github.mayki2964.UrlShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
